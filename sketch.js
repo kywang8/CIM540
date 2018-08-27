fb1ea4141bb4ffd7b058a68429e564787901a59c
kywang8 <35706598+kywang8@users.noreply.github.com>
2018-08-27 08:29:56 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(800, 400);
+}
+
+function draw() {
+  background(204);
+  ellipse (230, 100, 150, 60);
+  ellipse (230, 110, 80, 15);
+  ellipse (190, 70, 15, 15);
+  ellipse (270, 70, 15, 15);
+}
