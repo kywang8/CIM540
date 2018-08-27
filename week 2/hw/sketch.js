b9bfa915a0d08783799cb5428aa8c30313b39a66
kywang8 <35706598+kywang8@users.noreply.github.com>
2018-08-27 13:46:11 -0400

    week 2 hw

---
+++ week 2/hw/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(800, 400);
+  background(204);
+}
+
+function draw() {
+  fill(0, 0, 255);
+  ellipse (230, 100, 150, 60);
+  fill(255, 255, 0);
+  ellipse (230, 110, 80, 15);
+  fill(0, 191, 255);
+  ellipse (190, 70, 15, 15);
+  ellipse (270, 70, 15, 15);
+}
